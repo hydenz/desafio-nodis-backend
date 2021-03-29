94bd57969f3b8b7784104b0187670863f81ab1b9
hydenz <marcelof_lima@hotmail.com>
2021-03-29 14:30:51 -0300

    Consertado erro de digitação

--- db/knexfile.js
+++ db/knexfile.js
@@ -6,9 +6,10 @@ module.exports = {
   connection: {
     host: 'localhost',
     user: 'root',
-    database: (process.env.NODE_ENV = 'testing'
-      ? 'desafio-nodis-test'
-      : 'desafio-nodis'),
+    database:
+      process.env.NODE_ENV === 'testing'
+        ? 'desafio-nodis-test'
+        : 'desafio-nodis',
     timezone: 'UTC',
     typeCast: function (field, next) {
       if (field.type == 'DATETIME') {
